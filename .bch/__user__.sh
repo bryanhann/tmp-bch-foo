42fd5143e34a5f319610ba2b9532f87e6d33163a
Bryan Hann <nobody@nowhere>
2025-05-13 08:15:25 -0230

    Initial commit

---
+++ .bch/__user__.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo ++[${BASH_SOURCE[0]}] [$*]
+echo
+echo THIS __USER__ SCRIPT SHOULD BE MODIFIED.
+echo
+__path__    ${__rbin__}
+__export__  d_TEMP    /tmp/bozo-tmp.d
+__export__  f_FLAG    /tmp/bozo-tmp.d/flag
+echo --[${BASH_SOURCE[0]}] [$*]
+
---
+++ README.md
@@ -0,0 +1,15 @@
+# tmp-bch-foo
+
+version = 0.0.0
+
+## tmp bch-foo
+
+A demo
+
+## USE
+
+To setup:
+- Run: [./SETUP.sh]
+
+To use:
+- Run: [uv run tmp-bch-foo]
